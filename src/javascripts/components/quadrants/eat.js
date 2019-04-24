b34175a0ea7dd2da30ea4204d82462fb0a17f3ad
Mark Okafor <okaformark@gmail.com>
2019-04-24 18:55:06 -0500

    eat.js buttons work

--- src/javascripts/components/quadrants/eat.js
+++ src/javascripts/components/quadrants/eat.js
@@ -1,12 +1,41 @@
 import util from '../../helpers/util';
 
+let full = 50;
+
 const domStringBuilder = () => {
   let domString = '';
   domString += '<div class = "container">';
-  domString += '<button type="button">Health</button>';
-  domString += '<button type="button">Poison</button>';
+  domString += '<button id = "healthBtn" type="button">Health</button>';
+  domString += '<button id = "poisonBtn" type="button">Poison</button>';
   domString += '</div>';
   util.printToDom('eat', domString);
 };
 
-export default { domStringBuilder };
+
+const getFullValue = () => {
+  const fullValue = full;
+  console.error(fullValue);
+};
+
+const addHealth = () => {
+  if (full <= 90) {
+    full += 10;
+  }
+  getFullValue();
+};
+
+const subHealth = () => {
+  if (full >= 3) {
+    full -= 3;
+  }
+  getFullValue();
+};
+
+const addEventListener = () => {
+  const healthButton = document.getElementById('healthBtn');
+  healthButton.addEventListener('click', addHealth);
+  const poisonButton = document.getElementById('poisonBtn');
+  poisonButton.addEventListener('click', subHealth);
+};
+
+export default { domStringBuilder, getFullValue, addEventListener };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,6 +3,8 @@ import eat from './components/quadrants/eat';
 
 const init = () => {
   eat.domStringBuilder();
+  eat.addEventListener();
+  eat.getFullValue();
 };
 
 init();
