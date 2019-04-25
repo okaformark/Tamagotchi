1b7559249a8192ea5ae437c03ba4f463549d65bd
Mark Okafor <okaformark@gmail.com>
2019-04-24 20:11:12 -0500

    progress bar works

--- src/javascripts/components/quadrants/eat.js
+++ src/javascripts/components/quadrants/eat.js
@@ -1,25 +1,29 @@
 import util from '../../helpers/util';
 
-let full = 50;
+let full = 100;
 
 const domStringBuilder = () => {
   let domString = '';
   domString += '<div class = "container">';
+  domString += '<div id = "progressBar">';
+  domString += '<label for="file">Life:</label>';
+  domString += `<progress id="file" max="100" value=${full}>100%</progress>`;
   domString += '<button id = "healthBtn" type="button">Health</button>';
   domString += '<button id = "poisonBtn" type="button">Poison</button>';
   domString += '</div>';
+  domString += '</div>';
   util.printToDom('eat', domString);
 };
-
-
 const getFullValue = () => {
   const fullValue = full;
   console.error(fullValue);
+  util.printToDom('file', fullValue);
 };
 
 const addHealth = () => {
   if (full <= 90) {
     full += 10;
+    document.getElementById('file').value = full;
   }
   getFullValue();
 };
@@ -27,6 +31,7 @@ const addHealth = () => {
 const subHealth = () => {
   if (full >= 3) {
     full -= 3;
+    document.getElementById('file').value = full;
   }
   getFullValue();
 };
